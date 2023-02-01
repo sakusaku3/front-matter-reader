6f82fc2bec1689677b069bd64f97bed2b810c156
Sakuma Kota <haruichi33333@gmail.com>
2023-02-01 20:48:18 +0900

    initial commit

---
+++ reader.py
@@ -0,0 +1,131 @@
+import itertools
+import yaml
+import argparse
+from pathlib import Path
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument('func', choices=['tag', 'title'], help='機能')
+    p.add_argument('directory', help='検索対象ディレクトリ')
+    p.add_argument('-l', '--list', action='store_true', help='tagもしくはtitleリスト表示')
+    p.add_argument('--name', default='', help='検索対象のtagもしくはtitleを文字列指定')
+
+    args = p.parse_args()
+
+    headers = get_headers(args.directory)
+    if args.func == 'tag':
+        if args.list:
+            list_tags(headers)
+        else:
+            display_tag_files(headers, args.name)
+
+    elif args.func == 'title':
+        if (args.list):
+            list_titles(headers)
+
+def list_tags(headers):
+    tagsList = [header.tags for header in headers]
+    tags = itertools.chain.from_iterable(tagsList)
+
+    for tag in set(tags):
+        print(tag)
+
+def display_tag_files(headers, tagname):
+    for header in headers:
+        if tagname in header.tags:
+            print(header.filepath)
+
+def list_titles(headers):
+    for header in headers:
+        print(f'{header.title}:\t{header.filepath}')
+
+def get_headers(directory):
+    p = Path(directory)
+
+    for filepath in p.glob("*.md"):
+        lines = read_line_generator(filepath)
+        cleansedLines = remove_blank_line(lines)
+
+        if is_blank_file(cleansedLines):
+            continue
+
+        if not has_yaml_header(cleansedLines):
+            continue
+
+        headerLines = get_yaml_header_lines(cleansedLines)
+        headerText = "\n".join(list(headerLines))
+
+        header = yaml.safe_load(headerText)
+
+        tags = header.get("tags")
+
+        if not tags:
+            tags = []
+
+        if not isinstance(tags, list):
+            tags = tags.replace(' ', '').split(',')
+
+        yield Header(
+            filepath, 
+            header.get("uid"),
+            header.get("title"),
+            header.get("aliases"),
+            header.get("created"),
+            header.get("updated"),
+            tags)
+
+def remove_blank_line(lines):
+    return list(map(lambda x : x.rstrip(), filter(lambda x: x != "\n", lines)))
+
+def read_line_generator(filepath):
+    with open(filepath, encoding='utf-8') as file:
+        for line in file:
+            yield line
+
+def is_blank_file(lines):
+    return len(lines) == 0
+
+def has_yaml_header(lines):
+    if not "---" in lines[0]:
+        return False
+    
+    for line in lines[1:]:
+        if "---" in line:
+            return True
+    
+    return False
+
+def get_yaml_header_lines(lines):
+    if not "---" in lines[0]:
+        return False
+    
+    for line in lines[1:]:
+        if "---" in line:
+            break
+        else:
+            yield line
+
+class Header:
+
+    def __init__(
+        self, 
+        filepath, 
+        uid, 
+        title, 
+        aliases,
+        created,
+        updated,
+        tags):
+        self.filepath = filepath
+        self.uid = uid
+        self.title = title
+        self.aliases = aliases
+        self.created = created
+        self.updated = updated
+        self.tags = tags if tags else []
+
+    def conatins_title(self, title):
+        return title in self.title
+
+if __name__ == "__main__":
+    main()
---
+++ sample.md
@@ -0,0 +1,8 @@
+---
+uid: 20230123131313
+title: title
+aliases: [Title, TITLE]
+created: 2023-01-23 13:13:13
+updated: 2023-01-23 13:13:13
+tags: [tag1, tab2]
+---
