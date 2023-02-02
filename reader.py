50ccfab5a5bb49307a9371fc33e2d1d3043ca753
Sakuma Kota <haruichi33333@gmail.com>
2023-02-02 16:52:54 +0900

    ディレクトリ探索機能を追加

--- reader.py
+++ reader.py
@@ -8,11 +8,17 @@ def main():
     p.add_argument('func', choices=['tag', 'title'], help='機能')
     p.add_argument('directory', help='検索対象ディレクトリ')
     p.add_argument('-l', '--list', action='store_true', help='tagもしくはtitleリスト表示')
-    p.add_argument('--name', default='', help='検索対象のtagもしくはtitleを文字列指定')
+    p.add_argument('-n', '--name', default='', help='検索対象のtagもしくはtitleを文字列指定')
+    p.add_argument('-d', '--dir', action='store_true', help='ディレクトリを探索')
 
     args = p.parse_args()
 
-    headers = get_headers(args.directory)
+    headers = []
+    if args.dir:
+        headers = get_headers_from_two_layer(args.directory)
+    else:
+        headers = get_headers_from_one_layer(args.directory)
+
     if args.func == 'tag':
         if args.list:
             list_tags(headers)
@@ -39,7 +45,26 @@ def list_titles(headers):
     for header in headers:
         print(f'{header.title}:\t{header.filepath}')
 
-def get_headers(directory):
+def get_headers_from_two_layer(directory):
+    p = Path(directory)
+
+    dirs = [x for x in p.iterdir() if x.is_dir()]
+
+    for secondaryDirectory in dirs:
+        second = Path(secondaryDirectory)
+        for filepath in second.glob("*.md"):
+            lines = read_line_generator(filepath)
+            cleansedLines = remove_blank_line(lines)
+
+            if is_blank_file(cleansedLines):
+                continue
+
+            if not has_yaml_header(cleansedLines):
+                continue
+
+            yield get_header(cleansedLines, secondaryDirectory)
+
+def get_headers_from_one_layer(directory):
     p = Path(directory)
 
     for filepath in p.glob("*.md"):
@@ -52,27 +77,31 @@ def get_headers(directory):
         if not has_yaml_header(cleansedLines):
             continue
 
-        headerLines = get_yaml_header_lines(cleansedLines)
-        headerText = "\n".join(list(headerLines))
+        yield get_header(cleansedLines, filepath)
 
-        header = yaml.safe_load(headerText)
+def get_header(linesRemovedBlank, contentsPath):
+    headerLines = get_yaml_header_lines(linesRemovedBlank)
+    headerText = "\n".join(list(headerLines))
 
-        tags = header.get("tags")
+    header = yaml.safe_load(headerText)
 
-        if not tags:
-            tags = []
+    tags = header.get("tags")
 
-        if not isinstance(tags, list):
-            tags = tags.replace(' ', '').split(',')
+    if not tags:
+        tags = []
 
-        yield Header(
-            filepath, 
-            header.get("uid"),
-            header.get("title"),
-            header.get("aliases"),
-            header.get("created"),
-            header.get("updated"),
-            tags)
+    if not isinstance(tags, list):
+        tags = tags.replace(' ', '').split(',')
+
+    return Header(
+        contentsPath, 
+        header.get("uid"),
+        header.get("title"),
+        header.get("aliases"),
+        header.get("created"),
+        header.get("updated"),
+        tags)
+
 
 def remove_blank_line(lines):
     return list(map(lambda x : x.rstrip(), filter(lambda x: x != "\n", lines)))
